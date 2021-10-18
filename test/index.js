1d8737f0ad354623c4233c77953b948c2e5dcc25
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-18 14:35:33 +0800

    next

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
-const Util = require('../index.js');
+const util = require('../index.js');
 
-console.log(Util);
+console.log('Fallout util v' + util.version);
+
+var question = util.ask("What is your name? ");
+console.log("Your name is " + question);
