05931da055e72d8d32ba9c952cc7361be23097ea
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-18 14:56:33 +0800

    build error

--- test/index.js
+++ test/index.js
@@ -1,6 +1,4 @@
 const util = require('../index.js');
 
 console.log('Fallout util v' + util.version);
-
-var question = util.ask("What is your name? ");
-console.log("Your name is " + question);
+console.log(util);
