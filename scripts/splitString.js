03dd8f33439d67271f05b437cf254807b1b3fcb0
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-20 13:13:03 +0800

    logging string breh

--- scripts/splitString.js
+++ scripts/splitString.js
@@ -4,10 +4,8 @@ const replaceAll = require('./replaceAll');
 module.exports = (text = '', removeQuotations = false) => {
     let regex = new RegExp("(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
     text = text.toString().trim();
-    console.log(text)
     text = escapeRegExp(text);
     text = text.split(regex);
-    console.log(text)
  
     let newText = [];
     for (let value of text) {
