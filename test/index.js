f1d31799c2b5038d8f637ff26fd5f862ef0b250b
GhexterCortes <ghextershumies@gmail.com>
2022-03-15 22:22:03 +0800

    1.3.1

--- test/index.js
+++
@@ -1,6 +0,0 @@
-const Util = require('../index.js');
-
-const log = new Util.Logger('test');
-console.log(Util.input({ text: 'Hello! ', echo: '*', repeat: true, exitStrings: [] }));
-
-Util.makeSentence(['hello', 'world']);
