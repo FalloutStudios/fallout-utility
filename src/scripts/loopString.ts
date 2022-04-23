3612806b0f53649a0eaf57e620a5d8761d540ce8
GhexterCortes <ghextershumies@gmail.com>
2022-04-23 14:04:06 +0800

    deprecate loopString

--- src/scripts/loopString.ts
+++ src/scripts/loopString.ts
@@ -2,9 +2,13 @@
  * Loop string
  * @param num How many times to loop string
  * @param string String to loop
+ * @deprecated
  */
 export function loopString (num: number = 0, string: string = ''): string {
     let returnVal = '';
+
+    process.emitWarning(`loopString is deprecated. Use string.repeat instead.`, 'DeprecationWarning');
+
     for (let i = 0; i < num; i++) { returnVal += string; }
     return returnVal;
 }
