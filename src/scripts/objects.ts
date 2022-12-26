956e32eaa81e9e6de2ee684510f344e7dc427589
Ghex <ghextershumies@gmail.com>
2022-12-26 19:32:09 +0800

    update

--- src/scripts/objects.ts
+++ src/scripts/objects.ts
@@ -73,3 +73,15 @@ export function recursiveObjectReplaceValues<T extends string|{}|[]>(object: T, 
 
     return newObject as T;
 }
+
+/**
+ * Check if an object is from a class
+ * @param object Object to check
+ */
+function isClass<T>(object: any): object is T {
+    const isClassConstructor = object.constructor && object.constructor.toString().substring(0, 5) === 'class';
+    if (object.prototype === undefined) return isClassConstructor;
+
+    const isPrototypeClassConstructor = object.prototype.constructor && object.prototype.constructor.toString && object.prototype.constructor.toString().substring(0, 5) === 'class';
+    return isClassConstructor || isPrototypeClassConstructor;
+}
