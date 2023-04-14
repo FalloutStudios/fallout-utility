49e4c0937ac3ccaeb6f85b53958b32ef5b82a392
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-04-14 19:51:54 +0800

    Add partial deep

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,10 @@ export interface JSONEncodable<T> {
     toJSON(): T;
 }
 
+export type PartialDeep<T> = T extends object ? {
+    [P in keyof T]?: PartialDeep<T[P]>;
+} : T;
+
 export type RestOrArray<T> = T[]|T[][];
 export type Awaitable<T> = T|PromiseLike<T>;
 export type Noting<T> = T;
