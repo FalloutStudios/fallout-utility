c2460154840e4ec7127edf0fb7ebd23bb9ac7b02
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-04-19 13:03:21 +0800

    type

--- src/types.ts
+++ src/types.ts
@@ -15,3 +15,4 @@ export type PartialDeep<T> = T extends object ? {
 export type RestOrArray<T> = T[]|T[][];
 export type Awaitable<T> = T|PromiseLike<T>;
 export type Nothing<T> = T;
+export type Combine<A, B> = Omit<A, keyof B> & Omit<B, keyof A> & { [K in keyof A & keyof B]: A[K] | B[K] };
