656e4b594097f045f8ac90997507616d2c149229
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-04-24 14:09:31 +0800

    test

--- test/index.cjs
+++ test/index.cjs
@@ -1,12 +1,13 @@
 const kleur = require('kleur');
 const { Logger, LoggerLevel } = require('fallout-utility');
+const { inspect } = require('util');
 
 const logger = new Logger({
     name: 'Hi',
     formatMessageLines: {
-        [LoggerLevel.WARN]: message => kleur.red(message)
+        [LoggerLevel.ERROR]: msg => `${kleur.red('ERROR:')} ${msg}`
     }
 });
 
-logger.logFile('./logs/latest.log');
-logger.err(require('fallout-utility'));
+logger.err(kleur.blue('Hello!\nBitch\nLMFHD'), kleur.yellow(inspect(new Error('Hi'))));
+logger.err(kleur.red(inspect(new Error('Bitch'))));
