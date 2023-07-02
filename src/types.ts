9995fc8b4a8a2386c5bd81697847df19f81f11a2
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-07-02 11:24:41 +0800

    export type-fest

--- src/types.ts
+++ src/types.ts
@@ -8,11 +8,9 @@ export interface JSONEncodable<T> {
     toJSON(): T;
 }
 
-export type PartialDeep<T> = T extends object ? {
-    [P in keyof T]?: PartialDeep<T[P]>;
-} : T;
-
 export type RestOrArray<T> = T[]|T[][];
 export type Awaitable<T> = T|PromiseLike<T>;
 export type Nothing<T> = T;
 export type Combine<A, B> = Omit<A, keyof B> & Omit<B, keyof A> & { [K in keyof A & keyof B]: A[K] | B[K] };
+
+export * from 'type-fest';
