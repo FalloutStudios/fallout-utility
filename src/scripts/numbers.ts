d929bf4562ec281c6730a17e007c3ef8d25caace
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-11-04 19:19:22 +0800

    randint

--- src/scripts/numbers.ts
+++ src/scripts/numbers.ts
@@ -12,17 +12,24 @@ export function isNumber (value: unknown): boolean {
  * @param min Minimum value
  * @param max Maximum value
  */
-export function randomInt (min: number = 0, max: number = 0): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+export function randomInt (max: number): number;
+export function randomInt (min: number, max: number): number;
+export function randomInt (min: number, max?: number): number {
+    const { mx, mn } = {
+        mx: max === undefined ? min : max,
+        mn: max === undefined ? 0 : min
+    };
+
+    return Math.floor(Math.random() * (mx - mn + 1)) + mn;
 }
 
 /**
  * Formats number to K, M, B style
  * @param number Number to format
  */
-export function formatNumber(number: number): string {
+export function formatNumber(number: number, locale: string = 'en-US'): string {
     if (number >= 1000000000) return (number / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B';
     if (number >= 1000000) return (number / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
     if (number >= 1000) return (number / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
-    return String(number.toLocaleString('en-US'));
+    return number.toLocaleString(locale);
 }
