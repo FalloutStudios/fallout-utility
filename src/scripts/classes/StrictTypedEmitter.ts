556c36caffa8dc87e04c546b99178d723d1d1acb
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-05-23 14:33:53 +0800

    Added strict typed emitter

---
+++ src/scripts/classes/StrictTypedEmitter.ts
@@ -0,0 +1,34 @@
+import EventEmitter from 'node:events';
+import { EventEmitterOptions } from './TypedEmitter';
+
+export interface StringTypedEmitter<Events extends Record<string|symbol, any> = Record<string|symbol, any>> extends EventEmitter {
+    listenerCount(eventName: keyof Events): number;
+
+    listeners(eventName: keyof Events): Function[];
+
+    rawListeners(eventName: keyof Events): Function[];
+
+    removeAllListeners(event?: keyof Events): this;
+
+    off<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    on<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    once<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    addListener<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    emit<K extends keyof Events>(eventName: K, ...args: Events[K]): boolean;
+
+    prependListener<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    prependOnceListener<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+
+    removeListener<K extends keyof Events>(event: K, listener: (...args: Events[K]) => void): this;
+}
+
+export class StringTypedEmitter<Events extends Record<string|symbol, any>> extends EventEmitter {
+    constructor(options?: EventEmitterOptions) {
+        super(options);
+    }
+}
